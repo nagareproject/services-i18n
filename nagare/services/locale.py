4170b4f3d025068c425f5d6906a65a7cd41eca4a
Alain Poirier <alain.poirier@net-ng.com>
2018-12-03 18:34:23 +0100

    Simplifications

--- nagare/services/locale.py
+++ nagare/services/locale.py
@@ -11,16 +11,17 @@
 """
 
 from nagare.services import plugin
-from nagare.server import reference
 from nagare.i18n import set_locale, Locale, NegotiatedLocale
 
 
-class I18NBaseLocale(plugin.Plugin):
+class I18NLocale(plugin.Plugin):
     LOAD_PRIORITY = 80
     LOCALE_FACTORY = Locale
 
+    CONFIG_SPEC = {'dirname': 'string(default=None)'}
+
     def __init__(self, name, dist, dirname=None, i18n_service=None, **config):
-        super(I18NBaseLocale, self).__init__(name, dist)
+        super(I18NLocale, self).__init__(name, dist)
 
         self.dirname = dirname or i18n_service.output_directory
         self.config = config
@@ -32,29 +33,30 @@ class I18NBaseLocale(plugin.Plugin):
     def create_locale(self, **config):
         return self.LOCALE_FACTORY(dirname=self.dirname, **dict(self.config, **config))
 
-    def get_locale(self, **params):
-        raise NotImplementedError()
-
     def handle_request(self, chain, **params):
         locale = self.get_locale(**params)
         self.set_locale(locale)
 
         return chain.next(**params)
 
+    def get_locale(self, **params):
+        raise NotImplementedError()
+
 # -----------------------------------------------------------------------------
 
 
-class I18NPredefinedLocale(I18NBaseLocale):
-    CONFIG_SPEC = {
-        'language': 'string(default="en")',
-        'territory': 'string(default=None)',
-        'script': 'string(default=None)',
-        'variant': 'string(default=None)',
-        'dirname': 'string(default=None)',
-        'domain': 'string(default=None)',
-        'timezone': 'string(default=None)',
-        'default_timezone': 'string(default=None)'
-    }
+class I18NPredefinedLocale(I18NLocale):
+    CONFIG_SPEC = dict(
+        I18NLocale.CONFIG_SPEC,
+        language='string(default="en")',
+        territory='string(default=None)',
+        script='string(default=None)',
+        variant='string(default=None)',
+        dirname='string(default=None)',
+        domain='string(default=None)',
+        timezone='string(default=None)',
+        default_timezone='string(default=None)'
+    )
 
     def __init__(self, name, dist, services_service, **config):
         services_service(super(I18NPredefinedLocale, self).__init__, name, dist, **config)
@@ -67,30 +69,16 @@ class I18NPredefinedLocale(I18NBaseLocale):
 # -----------------------------------------------------------------------------
 
 
-class I18NLocale(I18NPredefinedLocale):
-    CONFIG_SPEC = {
-        'get_locale': 'string(default=None)',
-        'dirname': 'string(default=None)',
-    }
-
-    def __init__(self, name, dist, get_locale=None, dirname=None, services_service=None):
-        services_service(super(I18NLocale, self).__init__, name, dist, dirname=dirname, language='en')
-
-        if get_locale:
-            self.get_locale = reference.load_object(get_locale)[0].__get__(self, self.__class__)
-
-# -----------------------------------------------------------------------------
-
-
-class I18NNegociatedLocale(I18NBaseLocale):
-    CONFIG_SPEC = {
-        'locales': 'string_list(default=list())',
-        'default_locale': 'string(default="")',
-        'dirname': 'string(default=None)',
-        'domain': 'string(default=None)',
-        'timezone': 'string(default=None)',
-        'default_timezone': 'string(default=None)'
-    }
+class I18NNegociatedLocale(I18NLocale):
+    CONFIG_SPEC = dict(
+        I18NLocale.CONFIG_SPEC,
+        locales='string_list(default=list())',
+        default_locale='string(default="")',
+        dirname='string(default=None)',
+        domain='string(default=None)',
+        timezone='string(default=None)',
+        default_timezone='string(default=None)'
+    )
     LOCALE_FACTORY = NegotiatedLocale
 
     def __init__(
--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
     zip_safe=False,
     setup_requires=['setuptools_scm'],
     use_scm_version=True,
-    install_requires=('Babel>=2.5.0', 'pytz', 'nagare-services'),
+    install_requires=['Babel>=2.5.0', 'pytz', 'nagare-services'],
     entry_points='''
         [nagare.commands]
         i18n = nagare.admin.command:Commands
